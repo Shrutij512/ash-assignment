d6f29873dcacb31c2a252a96d4c1c33160649aba
Shruti Jain <jainshruti0512@gmail.com>
2024-03-21 21:37:03 +0530

    designer section respomsiveness change

--- src/Components/Designer.jsx
+++ src/Components/Designer.jsx
@@ -7,7 +7,7 @@ const Designer = () => {
                 <p className='text-green-600 text-5xl font-semibold'>Meet our Designer</p>
                 <p className='m-10 w-[70%] text-center ml-[15%]'>Lorem ipsum dolor sit amet consectetur. Proin sagittis duis et sed egestas turpis ut aliquet ultricies. Risus vulputate semper orci neque. Eu egestas et ac sit pellentesque nullam.</p>
             </div>
-            <div className='grid grid-cols-2 m-[10%] md:flex md:gap-5 md:m-10 lg:flex lg:gap-10 lg:m-20 '>
+            <div className='designer-container grid grid-cols-2 m-[10%] md:flex md:gap-5 md:m-10 lg:flex lg:gap-10 lg:m-20 '>
                 <div className='designer-div w-[28%] text-left pb-[3%]'>
                     <img src="https://1317092540.rsc.cdn77.org/MFbaA25USO6nYqs4gAdVd_zVUAk=/400x0/https://melbourne-university-publishing.s3.amazonaws.com/uploads/bf4/0dd/6fd/bf40dd6fd0148e423baf0117d1b2eab0bb967d88/K_Murphy.jpg" alt="" className='m-5 h-[70%] w-[80%] rounded-full' />
                     <p className='text-xl font-bold text-center'>Kathryn Murphy</p>
